86296e3aa2f915b838f5abad569cba680a150d20
karthikeyan001 <karthikmurugann@gmail.com>
2025-12-07 20:16:20 +0530

    Refactor KMeans fit and predict methods for clarity

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 class KMeans:
@@ -10,26 +9,36 @@ class KMeans:
 
     def fit(self, X):
         np.random.seed(self.random_state)
-        idx = np.random.choice(len(X), self.k, replace=False)
-        self.centroids = X[idx]
+
+        indices = np.random.choice(len(X), self.k, replace=False)
+        self.centroids = X[indices]
 
         for _ in range(self.max_iter):
-            distances = np.sqrt(((X[:, None] - self.centroids)**2).sum(axis=2))
-            labels = distances.argmin(axis=1)
 
-            new_centroids = np.array([X[labels == i].mean(axis=0) 
-                                      if np.any(labels == i) else self.centroids[i]
-                                      for i in range(self.k)])
+            distances = np.sqrt(
+                ((X[:, None, :] - self.centroids[None, :, :]) ** 2).sum(axis=2)
+            )
+            labels = np.argmin(distances, axis=1)
 
-            if np.max(np.linalg.norm(new_centroids - self.centroids, axis=1)) < self.tol:
-                break
+            new_centroids = np.array([
+                X[labels == i].mean(axis=0) if np.any(labels == i)
+                else self.centroids[i]
+                for i in range(self.k)
+            ])
 
+            shift = np.sqrt(((new_centroids - self.centroids) ** 2).sum(axis=1)).max()
             self.centroids = new_centroids
 
+            if shift < self.tol:
+                break
+
         self.labels_ = labels
-        self.inertia_ = ((X - self.centroids[labels])**2).sum()
+        self.inertia_ = ((X - self.centroids[labels]) ** 2).sum()
+
         return self
 
     def predict(self, X):
-        distances = np.sqrt(((X[:, None] - self.centroids)**2).sum(axis=2))
-        return distances.argmin(axis=1)
+        distances = np.sqrt(
+            ((X[:, None, :] - self.centroids[None, :, :]) ** 2).sum(axis=2)
+        )
+        return np.argmin(distances, axis=1)
