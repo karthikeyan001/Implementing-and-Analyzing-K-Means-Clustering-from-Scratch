b880ad01106e2ec311f919ff1cf22a4aad75ac86
karthikeyan001 <karthikmurugann@gmail.com>
2025-12-08 08:46:38 +0530

    Refactor silhouette score calculation for efficiency

--- silhouette_score_numpy.py
+++ silhouette_score_numpy.py
@@ -1,44 +1,20 @@
-
 import numpy as np
 
 def silhouette_score_numpy(X, labels):
-    X = np.asarray(X)
-    labels = np.asarray(labels)
     n = len(X)
-    unique_labels = np.unique(labels)
-
-    # Precompute distance matrix
-    dist_matrix = np.linalg.norm(X[:, None] - X[None, :], axis=2)
-
-    sil_scores = []
+    s = []
 
     for i in range(n):
-        same_cluster = labels == labels[i]
-        other_clusters = labels != labels[i]
+        same = X[labels == labels[i]]
+        others = [X[labels == lab] for lab in set(labels) if lab != labels[i]]
 
-        # a(i): mean intra-cluster distance
-        a = dist_matrix[i, same_cluster]
-        a = a[a != 0]  # remove distance to itself
-        a_i = a.mean() if len(a) > 0 else 0
+        a = np.mean(np.linalg.norm(same - X[i], axis=1)) if len(same) > 1 else 0
+        b = min(
+            np.mean(np.linalg.norm(group - X[i], axis=1))
+            for group in others
+        ) if others else 0
 
-        # b(i): smallest mean distance to any other cluster
-        b_i = np.inf
-        for lbl in unique_labels:
-            if lbl == labels[i]:
-                continue
-            cluster_points = labels == lbl
-            b = dist_matrix[i, cluster_points].mean()
-            b_i = min(b_i, b)
+        score = (b - a) / max(a, b) if max(a, b) > 0 else 0
+        s.append(score)
 
-        # silhouette(i)
-        s_i = (b_i - a_i) / max(a_i, b_i)
-        sil_scores.append(s_i)
-
-    return np.mean(sil_scores)
-
-
-if __name__ == "__main__":
-    # Example test
-    X = np.array([[1, 2], [2, 3], [10, 10], [11, 11]])
-    labels = np.array([0, 0, 1, 1])
-    print("Silhouette Score:", silhouette_score_numpy(X, labels))
+    return np.mean(s)
