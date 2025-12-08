6d5f47b09b37c1c163491842dc8700b19699faf7
karthikeyan001 <karthikmurugann@gmail.com>
2025-12-08 08:47:33 +0530

    Remove generate_dataset.py functionality
    Removed the entire generate_data function and its call.

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,16 +1,1 @@
 
-import numpy as np
-import pandas as pd
-
-def generate_data():
-    np.random.seed(42)
-    c1 = np.random.randn(100,2) + [0,0]
-    c2 = np.random.randn(100,2) + [5,5]
-    c3 = np.random.randn(100,2) + [-5,5]
-
-    X = np.vstack([c1,c2,c3])
-    df = pd.DataFrame(X, columns=["x","y"])
-    df.to_csv("data.csv", index=False)
-
-if __name__=="__main__":
-    generate_data()
